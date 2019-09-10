1504f4a738725741b54001847ac678febc1a5633
Ali Shehryar <ali.shehryar@live.com>
2019-09-10 20:00:00 +0500

    updated mics link

--- js/crosstab.js
+++ js/crosstab.js
@@ -5,7 +5,7 @@ var vizList = ["https://public.tableau.com/views/PovertyDataPortal-NEW1/Dashboar
         "https://public.tableau.com/views/PakistanPovertyIndicators/Poverty",
         "https://public.tableau.com/views/PopulationDistributionAcrossProvinces/Population-Dashboard",
         "https://public.tableau.com/views/Pakistan_Indicators/PSLM-Dashboard",
-        "https://public.tableau.com/profile/yahya.bajwa#!/vizhome/MICS/Dashboard1?publish=yes",
+        "https://public.tableau.com/views/MICS/Dashboard1?:embed=y&:display_count=yes&publish=yes&:origin=viz_share_link",
         "https://public.tableau.com/views/DistrictFragilityOverview/Dashboard1",
         "https://public.tableau.com/views/CrimesAgainstWomenFAFEN2010-12/CrimesAgainstWomen?:embed=y&:display_count=yes&:toolbar=no&:origin=viz_share_link",
         "https://public.tableau.com/shared/QBCZRW53B?:toolbar=no&:display_count=yes&:origin=viz_share_link",
